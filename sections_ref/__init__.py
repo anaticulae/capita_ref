3f9bbceeb6131dc5d77e8e42d56a4e9104e2a992
Helmut Konrad Schewe <helmutus@outlook.com>
2026-07-08 03:38:41 +0200

    chore(pyproject): add modern layout

--- sections_ref/__init__.py
+++ sections_ref/__init__.py
@@ -7,9 +7,10 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
+import importlib.metadata
 import os
 
-__version__ = '0.3.1'
+__version__ = importlib.metadata.version('capita_ref')
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PROCESS = 'sections_ref'
